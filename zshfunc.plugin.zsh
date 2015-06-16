679199d9e3d60f37629bb61fcef133b3bef044d3
fzerorubigd <fzerorubigd@gmail.com>
2015-06-16 11:43:56 +0430

    add history substring search

--- zshfunc.plugin.zsh
+++ zshfunc.plugin.zsh
@@ -47,3 +47,23 @@ uncaps() {
   xmodmap -e "remove Lock = Caps_Lock" # Remove Caps Lock
   xmodmap -e "keycode 66 = Multi_key" # Make the key into multykey
 }
+
+ if type history-substring-search-up > /dev/null; then  
+	# bind UP and DOWN arrow keys
+	zmodload zsh/terminfo
+	bindkey "$terminfo[kcuu1]" history-substring-search-up
+	bindkey "$terminfo[kcud1]" history-substring-search-down
+
+	# bind UP and DOWN arrow keys (compatibility fallback
+	# for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
+	bindkey '^[[A' history-substring-search-up
+	bindkey '^[[B' history-substring-search-down
+
+	# bind P and N for EMACS mode
+	bindkey -M emacs '^P' history-substring-search-up
+	bindkey -M emacs '^N' history-substring-search-down
+
+	# bind k and j for VI mode
+	bindkey -M vicmd 'k' history-substring-search-up
+	bindkey -M vicmd 'j' history-substring-search-down
+fi;
